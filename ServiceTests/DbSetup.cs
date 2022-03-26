6e7cff5d3087cf1ad9979b732360314d31b2777c
MorganDWalker <GreenPakman@gmail.com>
2022-03-26 10:18:19 -0400

    Imported service tests to this branch

---
+++ ServiceTests/DbSetup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIHLSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    public class DbSetup : IDisposable
+    {
+        protected DbContextOptions<PIHLDBContext> ContextOptions { get; }
+        public PIHLDBContext PIHLDBContext { get; set; }
+
+        public DbSetup()
+        {
+            ContextOptions = new DbContextOptionsBuilder<PIHLDBContext>()
+                .UseInMemoryDatabase(databaseName: "testing")
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            PIHLDBContext = new PIHLDBContext(ContextOptions);
+
+            PIHLDBContext.Database.EnsureDeleted();
+            PIHLDBContext.Database.EnsureCreated();
+
+            PIHLDBContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            // clean-up code
+        }
+    }
+}
